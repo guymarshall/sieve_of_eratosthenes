f01e0781e658f8cb664c4baf9d2f73d3791173db
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-11 12:25:54 +0000

    Add is_prime_number()

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ fn generate_vec(number: i32) -> Vec<i32> {
     (2..=number).collect()
 }
 
+pub fn is_prime_number(number: i32) -> bool {
+    if number % 2 == 0 {
+        return false;
+    }
+    let root_of_number: i32 = (number as f64).sqrt() as i32;
+    for i in (3..root_of_number).step_by(2) {
+        if number % i == 0 {
+            return false
+        }
+    }
+
+    return true;
+}
+
 fn main() {
     let user_input: i32 = 100;
 }

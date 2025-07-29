75559e1ee157ef01033e11e33f62bed248e412d4
Guy Marshall <guymarshall9@gmail.com>
2025-07-29 17:08:11 +0100

    UPDATE: Replace ArrayList<Boolean> with boolean[] to improve performance

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -12,27 +11,33 @@ public class Main {
             main(args);
         }
 
-        ArrayList<Boolean> is_prime = new ArrayList<>(limit + 1);
+        boolean[] is_prime = new boolean[limit + 1];
         for (int i = 0; i <= limit; i++) {
-            is_prime.add(true);
+            is_prime[i] = true;
         }
-        is_prime.set(0, false);
-        is_prime.set(1, false);
+        is_prime[0] = false;
+        is_prime[1] = false;
 
         for (int i = 2; i < limit; i++) {
             if (i * i > limit) {
                 break;
             }
-            if (is_prime.get(i)) {
+            if (is_prime[i]) {
                 int multiple = i * i;
                 while (multiple <= limit) {
-                    is_prime.set(multiple, false);
+                    is_prime[multiple] = false;
                     multiple += i;
                 }
             }
         }
 
-        long prime_count = is_prime.stream().filter(i -> i).count();
+        int prime_count = 0;
+        for (int i = 0; i < limit + 1; i++) {
+            if (is_prime[i]) {
+                prime_count++;
+            }
+        }
+
         System.out.printf("Number of primes up to %d: %d%n", limit, prime_count);
     }
 }
